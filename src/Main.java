31c792fbe834eae176af9f2dd268c52b266eaa9a
Nicholas Massa <nicholasmassa@kings.edu>
2019-12-20 05:27:02 -0500

    The test commit -featuring a hello world

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * This will represent the main class where the imageEditor will run.
+ * @author Nick
+ * @version 20 December 2019.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This will print out hello world and will be pushed into github using commands.");
+
+	}
+
+}
